8e8bb03353fb176f4b5aebd3f3394dd790b995c8
Mikdaam <mikdaam.badarou@edu.univ-eiffel.fr>
2022-05-17 19:26:19 +0200

    change the format of element in the file. And read the file to create the game cards

---
+++ src/fr/uge/splendor/utils/FileLoader.java
@@ -0,0 +1,46 @@
+package fr.uge.splendor.utils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import fr.uge.splendor.card.DevelopmentCard;
+import fr.uge.splendor.color.Color;
+import fr.uge.splendor.deck.CardDeck;
+import fr.uge.splendor.level.Level;
+
+public class FileLoader {
+  
+  private static HashMap<Color, Integer> parsePrice(String priceString) {
+    var price = new HashMap<Color, Integer>();
+    var pricesType = priceString.split("\\+");
+    
+    Arrays.stream(pricesType)
+          .forEach(element -> {
+            var info = element.split("_");
+            price.computeIfAbsent(Color.getColor(info[1]), val -> Integer.parseInt(info[0]));
+          });
+    
+    return price;
+  }
+  
+  public static CardDeck createCards(Path cardsFile) throws IOException {
+    var gameCards = new CardDeck();
+    
+     try (var csvReader = Files.newBufferedReader(cardsFile)) {
+       String line;
+       while ((line = csvReader.readLine()) != null) {
+         
+         System.out.println(line);
+         var cardComponents = line.split(",");
+         int cardPrestige = Integer.parseInt(cardComponents[2]);
+         var price = parsePrice(cardComponents[3]);
+         gameCards.add(new DevelopmentCard(Level.getLevel(cardComponents[0]), Color.getColor(cardComponents[1]), cardPrestige, price));
+       }
+     }
+     
+     return gameCards;
+  }
+}

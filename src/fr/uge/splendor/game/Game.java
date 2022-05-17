9bc24c4db665b5f19d5a1934bb2a08e6ffeacef6
Mikdaam <mikdaam.badarou@edu.univ-eiffel.fr>
2022-05-17 19:30:53 +0200

    Managing the game state

--- src/fr/uge/splendor/game/Game.java
+++ src/fr/uge/splendor/game/Game.java
@@ -1,9 +1,10 @@
 package fr.uge.splendor.game;
 
+import java.io.IOException;
+
 import fr.uge.splendor.deck.CardDeck;
 
 public interface Game {
-	CardDeck setupCards();
-	
+	CardDeck setupCards() throws IOException;
 	
 }
--- src/fr/uge/splendor/game/SimpleGame.java
+++ src/fr/uge/splendor/game/SimpleGame.java
@@ -1,12 +1,34 @@
 package fr.uge.splendor.game;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
 
+import fr.uge.splendor.board.Board;
 import fr.uge.splendor.deck.CardDeck;
+import fr.uge.splendor.deck.TokenDeck;
+import fr.uge.splendor.displayer.AsciiDisplayer;
+import fr.uge.splendor.displayer.Displayer;
+import fr.uge.splendor.utils.*;
+import fr.uge.splendor.level.Level;
+import fr.uge.splendor.player.Player;
 
 public class SimpleGame implements Game {
+  private final Board board;
+  private final HashMap<Level, CardDeck> decks;
+  private final TokenDeck tokens;
+  private final Player[] players;
+  private final Displayer displayer;
+  
+  public SimpleGame() {
+    this.board = null;
+    this.decks = null;
+    this.tokens = null;
+    this.players = null;
+    this.displayer = new AsciiDisplayer();
+  }
+  
+  
   public void shuffleCard() {
     
   }
@@ -18,17 +40,24 @@ public class SimpleGame implements Game {
   public void takeToken() {
     
   }
-
+  
+  public void doTour() {
+    /*
+     * Switch actions :
+     * 
+     * 
+     * */
+  }
+  
+  public void playersTour() {
+    /*forEach sur player
+     * 
+     * -> doTour(player);
+     * */
+  }
+  
   @Override
-  public CardDeck setupCards() {
-    try(var csvReader = new BufferedReader(new Path("cards.csv"))) {
-      var cards = new CardDeck();
-      var line = csvReader.readLine();
-      while(line != null) {
-        var values = line.split(",");
-        var card = new DevelopmentCard();
-      }
-
-	  return null;
+  public CardDeck setupCards() throws IOException {
+    return FileLoader.createCards(Path.of("res").resolve("base_game_cards.csv"));
   }
 }

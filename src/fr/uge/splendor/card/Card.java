da86cfd431af10e763fc82c347ea1f05224ef161
Mikdaam <mikdaam.badarou@edu.univ-eiffel.fr>
2022-05-17 19:23:39 +0200

    Change the order of Card info.

--- src/fr/uge/splendor/card/Card.java
+++ src/fr/uge/splendor/card/Card.java
@@ -3,15 +3,15 @@ package fr.uge.splendor.card;
 import java.util.HashMap;
 
 import fr.uge.splendor.color.*;
-import fr.uge.splendor.game.level.Level;
+import fr.uge.splendor.level.Level;
 
 public interface Card {
+	
+	Level level();
+	
+	Color color();
 	
 	int prestigePoint();
 	
 	HashMap<Color, Integer> price();
-	
-	Color color();
-	
-	Level level();
 }

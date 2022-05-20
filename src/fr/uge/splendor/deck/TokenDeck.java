8d1acc309d644112e5b15e0d4a113e4644e655a6
Mikdaam <mikdaam.badarou@edu.univ-eiffel.fr>
2022-05-20 18:44:08 +0200

    Added all the doc.

--- src/fr/uge/splendor/deck/TokenDeck.java
+++ src/fr/uge/splendor/deck/TokenDeck.java
@@ -4,12 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-import fr.uge.splendor.color.*;
-import fr.uge.splendor.token.*;
+import fr.uge.splendor.color.Color;
 
+/**
+ * This class represents a TokenDeck.
+ * It's represented by a HashMap that has Colors as keys and Integers as values.
+ * 
+ * @author Mikdaam Badarou
+ * @author Yunen Snacel
+ *
+ */
 public class TokenDeck {
   private final HashMap<Color, Integer> deck;
   
+  /**
+   * This constructor creates the HashMap that describes a TokenDeck and initializes it
+   * with all the colors and zero tokens in it.
+   */
   public TokenDeck() {
     this.deck = new HashMap<Color, Integer>();
     
@@ -17,15 +28,24 @@ public class TokenDeck {
                                .forEach(color -> deck.put(color, 0));
   }
  
-  public void add(Map<Token, Integer> tokens) {
-    Objects.requireNonNull(tokens, "You have to add at least 1 token, your list cannot be null!");
+  /**
+   * This method adds some tokens to a TokenDeck.
+   * 
+   * @param tokens - The HashMap describing the tokens to add to the TokenDeck.
+   */
+  public void add(Map<Color, Integer> tokens) {
+    Objects.requireNonNull(tokens, "You have to add at least 1 token, your map cannot be null!");
     
-    tokens.forEach((token, number) -> deck.merge(token.color(), number, Integer::sum));
+    tokens.forEach((color, number) -> deck.merge(color, number, Integer::sum));
   }
 
-  
+  /**
+   * This method removes some tokens from a TokenDeck.
+   * 
+   * @param tokens - The HashMap describing the tokens to remove from the TokenDeck.
+   */
   public void remove(Map<Color, Integer> tokens) {
-    Objects.requireNonNull(tokens, "Your list of tokens to remove cannot be null");
+    Objects.requireNonNull(tokens, "Your map of tokens to remove cannot be null");
     
     tokens.forEach((token, number) -> {
       
@@ -38,7 +58,11 @@ public class TokenDeck {
     });
   }
   
-  
+  /**
+   * This method creates the summary of a TokenDeck and computes it into an HashMap.
+   * 
+   * @return The HashMap describing the TokenDeck
+   */
   public HashMap<Color, Integer> getDeckSummary() {
     var res = new HashMap<Color, Integer>();
     
@@ -47,8 +71,14 @@ public class TokenDeck {
     return res;    
   }
   
-   
   
+  
+  
+  /**
+   * This method computes a TokenDeck's colors into a row for the TokenDeck's String format.
+   * 
+   * @return The colors computed into a row for the TokenDeck's String.
+   */
   private String colorsToString() {
     var sb = new StringBuilder();
     
@@ -65,7 +95,12 @@ public class TokenDeck {
     return sb.toString();
   }
 
-  
+  /**
+   * This method computes a TokenDeck's values (the number of tokens for each Color) into a row for
+   * the TokenDeck's String format.
+   * 
+   * @return The values computed into a row for the TokenDeck's String.
+   */
   private String valuesToString() {
     var sb = new StringBuilder();
     
@@ -78,8 +113,16 @@ public class TokenDeck {
     return sb.toString();
   }
   
-  
-  private String lineToString(String start, String mid, String end) {
+  /**
+   * This method creates a full row for a TokenDeck's String format.
+   * It takes in parameters a start, mid and end character to match the line's position.
+   * 
+   * @param start - The row's starting character
+   * @param mid - The row's mid character (between all cells)
+   * @param end - The row's ending character
+   * @return A TokenDeck's row computed with the given characters.
+   */
+  private String rowToString(String start, String mid, String end) {
     var sb = new StringBuilder(start);
     sb.append("───────────");
     
@@ -91,16 +134,19 @@ public class TokenDeck {
     return sb.toString(); 
   }
   
-  
-  @Override
+  /**
+   * This method returns the data of the TokenDeck into a fancy String format.
+   * 
+   * @return The TokenDeck computed into a String
+   */
   public String toString() {    
     var sb = new StringBuilder();
     
-    sb.append(lineToString("┌", "┬", "┐"))
+    sb.append(rowToString("┌", "┬", "┐"))
       .append(colorsToString())
-      .append(lineToString("├", "┼", "┤"))
+      .append(rowToString("├", "┼", "┤"))
       .append(valuesToString())
-      .append(lineToString("└", "┴", "┘"));
+      .append(rowToString("└", "┴", "┘"));
     
     return sb.toString();
   }

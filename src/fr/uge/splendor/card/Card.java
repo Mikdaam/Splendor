97af9406672dac439d14456c5b2d5c0ac23e6b0c
Mick Cool <bmikdaam@gmail.com>
2022-05-21 16:11:04 +0200

    move the method in the interface in order to display emptyCard.

--- src/fr/uge/splendor/card/Card.java
+++ src/fr/uge/splendor/card/Card.java
@@ -14,4 +14,14 @@ public interface Card {
 	int prestigePoint();
 	
 	HashMap<Color, Integer> price();
+	
+	static String emptyCardToString() {
+	  var sb = new StringBuilder();
+	  
+	  for (var i = 0; i < 8; i++) {
+	    sb.append("              \n");
+	  }
+	    
+	  return sb.append("              ").toString();
+	}
 }

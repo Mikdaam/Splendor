f28f77b345da2e6c91c0000861cd1119bb0a1d6a
yunensnacel <yunen.snacel@gmail.com>
2022-06-11 23:49:39 +0200

    Corrected the game's bugs so they can load the decks, display them and handle the boards correctly. Added a level to coordinates converter for more convenience.

--- src/fr/uge/splendor/game/NormalGame.java
+++ src/fr/uge/splendor/game/NormalGame.java
@@ -11,6 +11,7 @@ import java.util.EnumMap;
 import java.util.List;
 
 import fr.uge.splendor.board.Board;
+import fr.uge.splendor.card.Card;
 import fr.uge.splendor.card.Coordinate;
 import fr.uge.splendor.color.Color;
 import fr.uge.splendor.deck.CardDeck;
@@ -56,34 +57,40 @@ public class NormalGame implements Game {
 	private final Displayer displayer; /* View */
 	
   public NormalGame(int numberOfPlayers) {
-		if(numberOfPlayers < 2 && numberOfPlayers > 4) {
-			throw new IllegalArgumentException("Number of player should be greater than 2.");
-		}
-		
-		/*board = new Board(3, 4);
-    decks = new CardDeck[3];
-    tokens = new TokenPurse();
-    players = new Player[numberOfPlayers];
-    actions = new Action[6];*/
-		nbOfPlayers = numberOfPlayers;
-		displayer = new ConsoleDisplayer();
+  		if(numberOfPlayers < 2 && numberOfPlayers > 4) {
+  		  throw new IllegalArgumentException("Number of player should be greater than 2.");
+  		}
+  		
+  		/*board = new Board(3, 4);
+      decks = new CardDeck[3];
+      tokens = new TokenPurse();
+      players = new Player[numberOfPlayers];
+      actions = new Action[6];*/
+  		nbOfPlayers = numberOfPlayers;
+  		displayer = new ConsoleDisplayer();
     actions = new EnumMap<>(ActionType.class);
     
-    gameData = new GameData(new Board(3, 4), new EnumMap<>(Level.class), new Board(1, numberOfPlayers + 1), new TokenPurse(), new ArrayList<Player>(), new ConsoleDisplayer(), false);
+    gameData = new GameData(new Board(3, 4), new EnumMap<>(Level.class), new Board(1, numberOfPlayers + 1), new TokenPurse(), new ArrayList<Player>(), new ConsoleDisplayer(), new EnumMap<>(Level.class), false);
     
     if (numberOfPlayers == 2) {
-    	NUMBER_OF_TOKEN_MISSING_BY_NUM_OF_PLAYER = 3;
+    	 NUMBER_OF_TOKEN_MISSING_BY_NUM_OF_PLAYER = 3;
     } else if (numberOfPlayers == 3) {
-    	NUMBER_OF_TOKEN_MISSING_BY_NUM_OF_PLAYER = 2;
+    	 NUMBER_OF_TOKEN_MISSING_BY_NUM_OF_PLAYER = 2;
     } else {
-    	NUMBER_OF_TOKEN_MISSING_BY_NUM_OF_PLAYER = 0;
+    	 NUMBER_OF_TOKEN_MISSING_BY_NUM_OF_PLAYER = 0;
     }
-	}
+  }
+  
+  private void initLevelConverter() {
+    gameData.levelToInteger().put(Level.LEVEL_1, 2);
+    gameData.levelToInteger().put(Level.LEVEL_2, 1);
+    gameData.levelToInteger().put(Level.LEVEL_3, 0);
+  }
 	
-	private void initBoard() {
+	 private void initBoard() {
     for (int i = 1; i <= gameData.board().rows(); i++) {
-      for (int j = 1; j <= gameData.board().colums(); j++) {      	
-      	gameData.board().add(gameData.decks().get(Level.getLevel(i)).removeFirstCard(), new Coordinate(i, j));
+      for (int j = 1; j <= gameData.board().colums(); j++) {
+      	 gameData.board().push(gameData.decks().get(Level.getLevel(i)).removeFirstCard(), gameData.board().rows()-i);
       }
     }
   }
@@ -98,20 +105,22 @@ public class NormalGame implements Game {
     Path pathOfFile = Path.of("res").resolve("normal_game_cards.csv");
     var cardDeckByLevel = Game.setupCards(pathOfFile).groupByLevel();
     
-    /* TODO: Think different !!! */
     for (var level : levels) {
-			gameData.decks().put(level, cardDeckByLevel.get(level));
-		}
+      cardDeckByLevel.get(level).shuffleCardDeck();
+			   gameData.decks().put(level, cardDeckByLevel.get(level));
+		  }
     
     initNobles(cardDeckByLevel.get(Level.LEVEL_NOBLE));
   }
   
   private void initNobles(CardDeck nobles) {
-  	nobles.shuffleCardDeck();
-  	var len = gameData.noblesCards().colums();
-  	for (int i = 0; i < len; i++) {
-  		gameData.noblesCards().add(nobles.removeFirstCard(), new Coordinate(0, i));
-		}
+    nobles.shuffleCardDeck();
+    
+    var len = gameData.noblesCards().colums();
+    
+    for (var i = 0; i < len; i++) {
+  		  gameData.noblesCards().push(nobles.removeFirstCard(), 0);
+		  }
   }
   
   private void initTokens() {
@@ -131,7 +140,7 @@ public class NormalGame implements Game {
     tokens = tokens.addToken(Color.SAPPHIRE, numberOfTokenByColor);
     tokens = tokens.addToken(Color.GOLD, NUMBER_OF_GOLD_TOKEN);
     
-    gameData = new GameData(gameData.board(), gameData.decks(), gameData.noblesCards(), tokens, gameData.players(), displayer, gameData.actionSucceed());
+    gameData = new GameData(gameData.board(), gameData.decks(), gameData.noblesCards(), tokens, gameData.players(), displayer, gameData.levelToInteger(), gameData.actionSucceed());
   }
   
   /**
@@ -148,6 +157,7 @@ public class NormalGame implements Game {
   
   public void initGame() throws IOException {
   	initCardDecks();
+  	initLevelConverter();
 	  initBoard();
 	  initPlayers();
 	  initTokens();
@@ -171,306 +181,25 @@ public class NormalGame implements Game {
   private static List<Color> cardsColorsList() {
     return Color.getCardsColorsList();
   }
-  
-  
-  
-  
-  /* -- ACTIONS -- */
-  
-  /**
-   * This method checks if the given coordinates are correct in the context of a 2D array defined by
-   * its number of rows and columns.
-   * 
-   * @param coordinates - the coordinates to check.
-   * @param rows - the rows of the array we want to use the coordinates on.
-   * @param cols - the columns of the array we want to use the coordinates on.
-   * @return true if the coordinates are correct, false otherwise
-   */
-  /*private boolean checkCoordinates(Board board, Coordinate coordinate) {    
-    if (!board.rowIsInBoard(coordinate)) {
-    	displayer.displayActionError("You've entered a wrong row number");
-      return false;
-    } else if (!board.colIsInBoard(coordinate)) {
-    	displayer.displayActionError("You've entered a wrong column number");
-      return false;
-    }
-    
-    return true;
-  }*/
-  
-  
-  /**
-   * This method executes the action of buying a Card for a player, described by its ID.
-   * 
-   * @param playerID - The player's ID.
-   * @return true if the action has been performed correctly, false otherwise.
-   */
-  /*private boolean buyCard(int playerID) {
-    var coordinates = displayer.getCoordinates();
-    
-    if(!checkCoordinates(board, coordinates)) {
-      return false;
-    }
-    
-    var level = coordinates.row();
-    var card = board.remove(coordinates);
-    
-    if (players[playerID].canBuyCard(card)) {
-      tokens = tokens.add(players[playerID].buyCard(card, cardsColorsList()));
-      board.add(decks[level].removeFirstCard(), coordinates);
-    } else {
-      board.add(card, coordinates);
-      displayer.displayActionError("You are not able to buy the Card.");
-      return false;
-    }
-    
-    return true;
-  }*/
-  
-  /**
-   * This method executes the action of buying a Card for a player, described by its ID.
-   * 
-   * @param playerID - The player's ID.
-   * @return true if the action has been performed correctly, false otherwise.
-   */
-  /*private boolean buyReservedCard(int playerID) {
-    var coordinate = displayer.getCoordinates();
-    
-    /*!IMPORTANT: maybe not efficient ...*
-    var newReservedCards = new Board(1, players[playerID].numberOfReservedCards());
-    
-    if(!checkCoordinates(newReservedCards, coordinate)) {
-      return false;
-    }
-    
-    var card = players[playerID].removeFromReserved(coordinate);
-    
-    if (players[playerID].canBuyCard(card)) {
-      tokens = tokens.add(players[playerID].buyCard(card, cardsColorsList()));
-    } else {
-      players[playerID].pushToReserved(card);
-      displayer.displayActionError("You are not able to buy the Card.");
-      return false;
-    }
-    
-    return true;
-  }*/
-  
-  /*private void givePlayerGoldToken(int playerID) {
-    if (tokens.getColorNumber(Color.GOLD) > 0) {
-      players[playerID].takeToken(Color.GOLD);
-      tokens = tokens.remove((new TokenPurse()).addToken(Color.GOLD, 1));
-    }
-  }*/
-  
-  /*private boolean reserveCardBoard(int playerID) {
-    var coordinate = displayer.getCoordinates();
-    
-    if(!checkCoordinates(board, coordinate)) {
-      return false;
-    }
-    
-    var level = coordinate.row();
-    var card = board.remove(coordinate);
-    
-    if (players[playerID].numberOfReservedCards() < 3) {
-      players[playerID].pushToReserved(card);
-      givePlayerGoldToken(playerID);
-      board.add(decks[level].removeFirstCard(), coordinate);
-    } else {
-      board.add(card, coordinate);
-      displayer.displayActionError("You cannot reserve any more Card.");
-      return false;
-    }
-    
-    return true;
-  }*/
-  
-  
-  /*private boolean checkDeckLevel(Level level) {
-    if (level != Level.LEVEL_1 && level != Level.LEVEL_2 && level != Level.LEVEL_3) {
-      displayer.displayActionError("The level you have chosen doesn't exist.");
-      return false;
-    }
-    
-    return true;
-  }
-  
-  private int convertLevelToIndex(Level level) {
-    if (level == Level.LEVEL_1) {
-      return 2;
-    } else if (level == Level.LEVEL_2) {
-      return 1;
-    } 
-    
-    return 0;
-  }
-  
-  private boolean reserveCardDeck(int playerID) {
-    var level = displayer.getDeckLevel();
-    
-    if(!checkDeckLevel(level)) {
-      return false;
-    }
-    
-    var card = decks[convertLevelToIndex(level)].removeFirstCard();
-    
-    if (players[playerID].numberOfReservedCards() < 3) {
-      players[playerID].pushToReserved(card);
-      givePlayerGoldToken(playerID);
-    } else {
-      decks[convertLevelToIndex(level)].add(card);
-      displayer.displayActionError("You cannot reserve any more Card.");
-      return false;
-    }
-    
-    return true;
-  }*/
-  
-  /**
-   * This method checks if a player has 10 tokens or less.
-   * 
-   * @param playerID - the player's ID
-   * @return true if the player has less than 10 tokens, false otherwise.
-   */
-  /*private boolean checkPlayersTokensNumber(int playerID) {
-    if (players[playerID].getNumberOfTokens() == 10) {
-      /*Exchange the tokens maybe?*
-      displayer.displayActionError("You already have 10 tokens. You cannot get more than that.");
-      return false;
-    }
-    
-    return true;
-  }*/
-  
-  /**
-   * This method checks if there is at least 1 token of the given Color in the TokenDeck.
-   * 
-   * @param color - the color of the token we want to check on.
-   * @return true if the color has at least 1 token, false otherwise.
-   */
-  /*private boolean checkTokensNumber(Color color) {
-    if (tokens.getColorNumber(color) <= 0) {
-      displayer.displayActionError("Not enough tokens to perform the action.");
-      return false;
-    }
-    
-    return true;
-  }*/
-  
-  /**
-   * This method checks if the color isn't an UNKNOWN Color.
-   * 
-   * @param color - the Color to check.
-   * @return true if the color is not UNKNOWN, false otherwise.
-   */
-  /*private boolean checkTokenColor(Color color) {
-    if (color == Color.UNKNOWN || color == Color.GOLD) {
-      displayer.displayActionError("Unknown Color.");
-      return false;
-    }
-    
-    return true;
-  }*/
-  
-  /**
-   * This method checks, for a list of tokens' colors, if the color is well defined
-   * and if there is at least 1 token of this color in the TokenDeck.
-   * 
-   * @param colors - the list of colors to check
-   * @return true if the list is correct, false otherwise.
-   */
-  /*private boolean checkTokensColorsList(List<Color> colors) {
-    for (var color : colors) {
-      if (!checkTokenColor(color) || !checkTokensNumber(color)) {
-        return false;
-      }
-    }
-    
-    return true;
-  }*/
-  
-  /**
-   * This method checks if a list of colors effectively contains 3 distinct colors.
-   * 
-   * @param colors - the list of colors to check.
-   * @return true if there is three distinct colors in the list, false otherwise.
-   */
-  /*private boolean checkThreeDistinctColors(List<Color> colors) {
-    if(colors.stream().distinct().count() != 3) {
-      displayer.displayActionError("You have not entered three distinct colors");
-      return false;
-    }
-    
-    return true;
-  }*/
-  
-  /**
-   * This method executes the action of taking two tokens of the same color for a player, described by its ID.
-   * 
-   * @param playerID - The player's ID.
-   * @return true if the action has been performed correctly, false otherwise.
-   */
-  /*private boolean takeThreeTokens(int playerID) {
-    var colors = displayer.getThreeColor();
-    if (!checkPlayersTokensNumber(playerID) || !checkThreeDistinctColors(colors) || !checkTokensColorsList(colors)) {
-      return false;
-    }
-    
-    for (var color : colors) {
-     	if(players[playerID].getNumberOfTokens() < 10) {
-      		players[playerID].takeToken(color);
-      		tokens = tokens.remove((new TokenPurse()).addToken(color, 1));
-     	}			
-		}
-    
-    return true;
-  }*/
-  
-  /**
-   * This method executes the action of taking three tokens fo different colors each for a player, described by its ID.
-   * 
-   * @param playerID - The player's ID.
-   * @return true if the action has been performed correctly, false otherwise.
-   */
-  /*private boolean takeTwoTokens(int playerID) {
-    var color = displayer.getUniqueColor();
-    if (!checkPlayersTokensNumber(playerID) || !checkTokenColor(color) || !checkTokensNumber(color)) {
-      return false;
-    }
-    
-    var toTake = 2;
-    if (tokens.getColorNumber(color) < 4) { /*Not enough tokens to take two of them*
-      toTake = 1;
-    }
-    
-    var given = 0; /*To check how much we gave*
-    while(given < toTake && players[playerID].getNumberOfTokens() < 10) {
-      players[playerID].takeToken(color);
-      given++;
-    }
-    
-    tokens = tokens.remove((new TokenPurse()).addToken(color, given));
-    
-    if (given == 0) {
-      return false;
-    }
-    
-    return true;
-  }*/
+
   
   private void visitOfNobles(int playerID) {
-  	for (int i = 0; i < gameData.noblesCards().colums(); i++) {
-  		var coordinate = new Coordinate(0, i);
-    	var noble = gameData.noblesCards().remove(coordinate);
-    	if (gameData.players().get(playerID).canGetNoble(noble)) {
-    		gameData.players().get(playerID).buyCard(noble, Color.getCardsColorsList());
-    		break;
-    	} else {
-    		gameData.noblesCards().add(noble, coordinate);
-    	}
-		}
-	}
+    var noblesList = new ArrayList<Card>();
+    var numberOfNobles = gameData.noblesCards().numberOfCards();
+    for (var i = 0; i < numberOfNobles; i++) {
+      noblesList.add(gameData.noblesCards().remove(new Coordinate(0, 0)));
+    }
+    
+    var hasNoble = false;
+    for (var noble: noblesList) {
+      if (!hasNoble && gameData.players().get(playerID).canGetNoble(noble)) {
+        gameData.players().get(playerID).buyCard(noble, Color.getCardsColorsList());
+        hasNoble = true; /*The player can only get one noble per turn, this will prevent them from getting another noble*/
+      } else {
+        gameData.noblesCards().push(noble, 0); /*We put all the other nobles back in the data*/
+      }
+    }
+	 }
   
   /**
    * This method allows the player, described by its ID, to choose an Action.
@@ -499,7 +228,7 @@ public class NormalGame implements Game {
       	gameData = actions.get(choosenActionType).apply(playerID, gameData);
       }
 		}
-    gameData = new GameData(gameData.board(), gameData.decks(), gameData.noblesCards(), gameData.tokens(), gameData.players(), displayer, false);
+    gameData = new GameData(gameData.board(), gameData.decks(), gameData.noblesCards(), gameData.tokens(), gameData.players(), displayer, gameData.levelToInteger(), false);
   }
   
   public GameData getGameState() {
